cd90c3b6228d7b3c4a69ba0b9b492cd25dcc5626
NancyyKin <93944912+NancyyKin@users.noreply.github.com>
2024-07-10 22:39:34 -0500

    added edit contact page

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
+import EditScreen from './screens/EditScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,12 +13,19 @@ const App = () => {
   return (
       <NavigationContainer>
         <Stack.Navigator>
+
           <Stack.Screen name="Home">
             {props => <HomeScreen {...props} contacts={contacts} setContacts={setContacts} />}
           </Stack.Screen>
+
           <Stack.Screen name="Details">
             {props => <DetailsScreen {...props} contacts={contacts} setContacts={setContacts} />}
           </Stack.Screen>
+
+          <Stack.Screen name="Edit Contact">
+            {props => <EditScreen {...props} contacts={contacts} setContacts={setContacts} />}
+          </Stack.Screen>
+
         </Stack.Navigator>
       </NavigationContainer>
   );
